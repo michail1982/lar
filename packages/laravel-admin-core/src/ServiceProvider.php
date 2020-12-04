7bb11eacc93612b81f0e6d7a70ba734f0df99afe
Максаров Михаил <mikhail.maksarov@brander.ua>
2020-12-04 18:15:43 +0200

    Init

---
+++ packages/laravel-admin-core/src/ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Michail1982\AdminCore;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
